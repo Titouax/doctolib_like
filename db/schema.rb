7f8dd88de2fb24b5ba4afc564e92b63bcbab9c74
Titouax <titouan.dessus@gmail.com>
2018-07-26 19:08:30 +0200

    Pimp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_26_123108) do
+ActiveRecord::Schema.define(version: 2018_07_26_165746) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "date"
@@ -18,22 +18,46 @@ ActiveRecord::Schema.define(version: 2018_07_26_123108) do
     t.integer "patient_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_appointments_on_city_id"
     t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
     t.index ["patient_id"], name: "index_appointments_on_patient_id"
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string "city_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "doctors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "speciality"
     t.integer "postal_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_doctors_on_city_id"
+  end
+
+  create_table "doctors_specialities", id: false, force: :cascade do |t|
+    t.integer "doctor_id", null: false
+    t.integer "speciality_id", null: false
+    t.index ["doctor_id", "speciality_id"], name: "index_doctors_specialities_on_doctor_id_and_speciality_id"
+    t.index ["speciality_id", "doctor_id"], name: "index_doctors_specialities_on_speciality_id_and_doctor_id"
   end
 
   create_table "patients", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_patients_on_city_id"
+  end
+
+  create_table "specialities", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,22 +10,46 @@ require 'faker'
 
 10.times do |index|
 
+  City.create(
+    city_name: Faker::Name.first_name
+  )
+
   Patient.create(
     first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name
+    last_name: Faker::Name.last_name,
+    city_id: rand(City.first.id..City.last.id)
   )
 
   Doctor.create(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    speciality: Faker::Cannabis.category,
-    postal_code: Faker::Address.zip
+    postal_code: Faker::Address.zip,
+    city_id: rand(City.first.id..City.last.id)
   )
 
   Appointment.create(
     date: Faker::Date.backward(30),
     patient_id: rand(Patient.first.id..Patient.last.id),
-    doctor_id: rand(Doctor.first.id..Doctor.last.id)
+    doctor_id: rand(Doctor.first.id..Doctor.last.id),
+    city_id: rand(City.first.id..City.last.id)
   )
+
+  Speciality.create(
+    name: Faker::Name.first_name
+  )
+
+  # these two last commands fill up the assemblies_parts table
+  # give each part a random assembly
+  Doctor.all.each do |doctor|
+   doctor.speciality.push(Speciality.all.sample)
+   doctor.save
+  end
+
+  # give each assembly a random part
+  Speciality.all.each do |speciality|
+   speciality.doctors.push(Doctor.all.sample)
+   speciality.save
+  end
+
 
 end
